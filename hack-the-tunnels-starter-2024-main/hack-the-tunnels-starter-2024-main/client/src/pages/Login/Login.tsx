9bb41e7832a8c3319480a5adf9b910f3da18eb57
Zesan <zesanrahim@gmail.com>
2024-10-19 12:29:18 -0400

    login page

--- hack-the-tunnels-starter-2024-main/hack-the-tunnels-starter-2024-main/client/src/pages/Login/Login.tsx
+++ hack-the-tunnels-starter-2024-main/hack-the-tunnels-starter-2024-main/client/src/pages/Login/Login.tsx
@@ -7,7 +7,7 @@ function Login() {
   const [message, setMessage] = useState(null);
   const { login } = useAccountContext();
 
-  const attemptLogin = async () => {
+  const attemptLogin = async () => { //test
     try {
       const message = await login("admin@email.com", "password");
       setMessage(message);
