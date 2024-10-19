9906574c2511278af4d27ea13eb03edfdbb71e32
rieley <rieleyhunt@cmail.carleton.ca>
2024-10-19 12:31:19 -0400

    Added Rick Roll to 404

--- hack-the-tunnels-starter-2024-main/hack-the-tunnels-starter-2024-main/client/src/pages/NotFound/NotFound.tsx
+++ hack-the-tunnels-starter-2024-main/hack-the-tunnels-starter-2024-main/client/src/pages/NotFound/NotFound.tsx
@@ -5,6 +5,15 @@ function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
       <h1>404</h1>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </Layout>
   );
 }
